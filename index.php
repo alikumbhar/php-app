23c70d990c8e47ef3e82ff73ef7bb8df796a3da3
alikumbhar <ali_kumbhar@hotmail.com>
2025-03-23 23:19:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <he>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Information</title>
+    <title>Student Informations</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 <style>
