75942fc1ddb222e4bac68808684d9c3409718af1
Ali Ahmed <ali_kumbhar@hotmail.com>
2025-08-29 20:02:07 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <he>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SMS</title>
+    <title>SMS Docker Testing</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 <style>
